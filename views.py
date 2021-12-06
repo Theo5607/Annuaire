4d3cc3d5553bc45005a346d40f116a6dc899e621
Theo5607 <53973847+Theo5607@users.noreply.github.com>
2021-12-06 11:52:37 +0100

    Update views.py

--- views.py
+++ views.py
@@ -30,7 +30,23 @@ def recherche_resultats():
   n = result['nom']
   p = result['prenom']
   nt = result['numero']
-  recherche_bd((n, p, nt))
+  liste_contacts=recherche_bd((n, p, nt))
+  return render_template("recherche_resultats.html", liste_c=liste_contacts)
+
+@app.route('/liste_contacts')
+def liste_contacts():
+    return render_template("liste_contacts.html", liste_c=recup_donnees)
+
+@app.route('/suppr_contact_choix')
+def suppr_contact_choix():
+    return render_template("suppr_contacts_choix.html", liste_c=recup_donnees)
+
+@app.route('/suppr_contact',methods = ['POST'])
+def suppr_contact():
+    result = request.form
+    id_c = result['id_c']
+    suppr_contact(id_c)
+    return render_template("suppr_ok.html", id_contact=id_c)
 
 #fonctions base de données
 
@@ -48,15 +64,32 @@ def recherche_bd(values):
     conn = sqlite3.connect('baseDonnees.db')
     cur = conn.cursor()
     data=[]
-    for i in range(3):
-      if value(i)!='':
-        if i==0:
-          cur.execute("SELECT name, prenom, numero FROM NUMEROS WHERE nom = ?",(values[0],))
-          mdp_sing=cur.fetchall()
-        elif i==1:
-          
+    cur.execute("SELECT name, prenom, numero FROM NUMEROS WHERE nom = ?",(values[0],))
+    data.append(cur.fetchall())
+    cur.execute("SELECT name, prenom, numero FROM NUMEROS WHERE prenom = ?",(values[1],))
+    data.append(cur.fetchall())
+    cur.execute("SELECT name, prenom, numero FROM NUMEROS WHERE numero = ?",(values[2],))
+    data.append(cur.fetchall())
     
     cur.close()
     conn.close()
+    
+    return list(set(data))
 
+def suppr_contact(id_c):
+    conn = sqlite3.connect('baseDonnees.db')
+    cur = conn.cursor()
+    cur.execute("DELETE FROM NUMEROS WHERE id = ?", (id_c,))
+    cur.close()
+    conn.close()
+
+def recup_donnees():
+    conn = sqlite3.connect('baseDonnees.db')
+    cur = conn.cursor()
+    cur.execute("SELECT name, prenom, numero FROM NUMEROS")
+    cur.close()
+    conn.close()
+    
+    return cur.fetchall()
+    
 app.run(port=5000)
